bdfa340ab4318c4e1f970387fa56b60dec58e097
Steve Benchik <stevebenchik@gmail.com>
2017-05-30 11:13:42 -0400

    Bind value form to state and handle change

--- src/Github.js
+++ src/Github.js
@@ -3,13 +3,23 @@ import React, { Component } from 'react'
 import './Github.css'
 
 class Github extends Component{
+    
+    state = {
+        username: '',
+    }
+
+    handleChange = (ev) => {
+        const username = ev.currentTarget.value
+        this.setState({ username }, () => console.log(this.state))
+    }
+
     render(){
         return(
             <div className="github">
                 <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"/>
                 <form>
                     <div>
-                        <input type="text" />
+                        <input type="text" value={this.state.username} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <button type="submit">Look up a github user</button>
